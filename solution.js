6a36012922689f79df26621237af864e976f80a3
Jesus Morales <jesusmorales7499@gmail.com>
2022-06-29 21:27:52 -0400

    finish hw

--- solution.js
+++ solution.js
@@ -1,6 +1,11 @@
 // You must RETURN your result in each function!
 
 function printNums(nums) {
+  const newArr =[]
+  nums.forEach((element) => {
+    newArr.push(element)
+  })
+  return newArr
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
@@ -8,6 +13,12 @@ function printNums(nums) {
 }
 
 function returnSums(nums) {
+  const newArr = []
+  nums.forEach((n, index) => {
+    newArr.push(n + index)
+
+  })
+  return newArr
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
@@ -16,6 +27,11 @@ function returnSums(nums) {
 }
 
 function returnTotal(objs) {
+  let objSum = 0
+  objs.forEach((element) => {
+    objSum += element.n
+  })
+  return objSum
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
@@ -24,18 +40,49 @@ function returnTotal(objs) {
 }
 
 function printMoney(decimals) {
-  /**
-   * @param decimals => [2.30, 2.40, 32.99]
-   * Using .map, return an array of decimals formatted as dollars.
-   * EX: {
-   * input: [12.34, 9.99],
-   * output: ['$12.34', '$9.99']
-   * }
-   * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
-   */
-}
+/**   * @param decimals => [2.30, 2.40, 32.99]   * Using .map, return an array of decimals formatted as dollars.   * EX: {   * input: [12.34, 9.99],   * output: ['$12.34', '$9.99']   * }   * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)   */
+        // @param decimals   
+  const newNum = decimals.map(value => {
+    let newString = value.toFixed(2)
+    newString = newString.toString()
+    let dollarString = "$" + newString
+    return dollarString
+
+
+
+    })
+    return newNum
+
+
+
+ }
+
+
+
+
+
+/**   *=> [2.30, 2.40, 32.99]   * Using .map, return an array of decimals formatted as dollars.   * EX: {   * input: [12.34, 9.99],   * output: ['$12.34', '$9.99']   * }   * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)   */
+        // @param decimals 
+  
+ 
+
+  //=> [2.30, 2.40, 32.99]
+//    * Using .map, return an array of decimals formatted as dollars.
+//    * EX: {
+//    * input: [12.34, 9.99],
+//    * output: ['$12.34', '$9.99']
+//    * }
+//    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
+//    */
+
 
 function returnAllTrueValues(values) {
+  const newNum = values.filter((element) => {
+    if (element.happy === true) {
+      return element
+    }
+  })
+  return newNum
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
@@ -43,6 +90,13 @@ function returnAllTrueValues(values) {
 }
 
 function addTwo(nums) {
+  const addTwo = nums.map((num) => {
+    return num + 2
+  }).reduce((acc, value)=>{
+    return acc + value
+  })
+return addTwo
+  
   /**
    * @param nums => [1,2,3,4]
    * Using .reduce, return the total sum of all numbers.
@@ -53,6 +107,12 @@ function addTwo(nums) {
 }
 
 function joinStrings(strings) {
+  const newString = strings.reduce((acc, value)=> {
+    return acc + value
+  })
+
+  return newString
+
   /**
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
